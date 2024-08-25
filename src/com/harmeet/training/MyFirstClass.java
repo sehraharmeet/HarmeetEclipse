5839103c01bf3bdd5e33e5729b47d3fae0bc1fee
sehraharmeet <150445048+sehraharmeet@users.noreply.github.com>
2024-08-25 10:56:22 +0530

    Diya User
    new changes for Diya

---
+++ src/com/harmeet/training/MyFirstClass.java
@@ -0,0 +1,11 @@
+package com.harmeet.training;
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 500; i++) {
+			System.err.println("Sqr(" + i + ") = " + Math.pow(i, 2) );
+		}
+	}
+
+}
